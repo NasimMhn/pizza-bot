1fc611024ae3032386004dd46c8076330bc65ed8
nasimmhn <nasim.mahzoun@gmail.com>
2019-10-15 17:33:13 +0200

    Co-authored-by: swolfson1 <swolfson23@gmail.com> interation1-

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -2,6 +2,8 @@ const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
+
 const pizzaPrice = 80
+console.log(vegetarian,hawaiian, pepperoni )
 
 //Put your Javscript code here:

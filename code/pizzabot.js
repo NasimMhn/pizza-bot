083f8bd72a4bf41303d0a61d05bb5db04b9e3f2a
nasimmhn <nasim.mahzoun@gmail.com>
2019-10-17 02:49:25 +0200

    added some comments

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,32 +1,16 @@
 const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
-
 const pizzaPrice = 80
-console.log(vegetarian,hawaiian, pepperoni )
 
 
-// checks order total
-const orderTotal = (orderQuantity, pizzaPrice) => { 
-    return (orderQuantity * pizzaPrice)
-} 
 
-// checks cooking times
-const cookingTime = (orderQuantity) => {
-    let orderTime
-    
-    if (orderQuantity <= 2 ) 
-       { orderTime = 10;
-    } else if (orderQuantity >= 6){
-        orderTime = 20;
-    } else {
-    orderTime = 15;
-    } 
-    return orderTime;
-}
+//Iteration2 - GREETING THE CUSTOMER 
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian},${hawaiian} and ${pepperoni}`)
 
-//  when checkorderName goes into the function trick the function to exit and go to validate var valid which checks if the pizzaName is on the menu.  
 
+//Iteration 5 - checking that the pizza is on the menu    //Iteration6 - part1
+//when checkorderName goes into the function trick the function to exit and go to validate var valid which checks if the pizzaName is on the menu.  
 const checkOrderName = (orderName) =>{
     if (orderName==vegetarian || orderName==hawaiian || orderName == pepperoni) {
         return false
@@ -35,29 +19,46 @@ const checkOrderName = (orderName) =>{
         return true
     }
 }
-
-// GREETING THE CUSTOMER
-
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian},${hawaiian} and ${pepperoni}`)
-
-
-// valid makes the user put in a valid menu chocie to exit the while loop we return false in the checkOrderName
-let valid = true
-while (valid) {
-    var orderName = prompt(`Enter the name of the pizza you want to order today! our options are Vegetarian,Hawaiian, or Pepperoni`) 
+// valid makes the user put in a valid menu chocie to exit the while loop we return false in the checkOrderName.
+    let valid = true
+    while (valid) {
+    //Iteration3 - Ask the customer 
+    var orderName = prompt(`Enter the name of the pizza you want to order today!`) 
+    
     valid = checkOrderName(orderName)
 }
 
-//
-var orderQuantity = Number(prompt(`How many of ${orderName} would you like?`))
+
+//Iteration3 - Ask the customer 
+let orderQuantity = prompt(`How many of ${orderName} would you like?`)
 
 
-// Iteration 5 Checking that the Pizza is on the menu
+//Iteration4 - Finalize the order   //Itration6 - part2
+const totalCost = (orderQuantity) => { 
+    return orderQuantity * pizzaPrice
+} 
+
+let totalPrice = totalCost(orderQuantity)
+
+
+
+//Iteration5 - show cooking times    //Iteration6 - part3 
+const cookingTime = (orderQuantity) => {
+    
+    if (orderQuantity <= 2 ) { 
+        return 10
+    } else if (orderQuantity >= 6){
+       return 20
+    } else {
+        return 15
+   
+    } 
+}
+
 let orderTime = cookingTime(orderQuantity)
 
-//Iteration 4 - Finalize the order
 
-let totalPrice = orderTotal(orderQuantity, pizzaPrice)
+//Final message
 alert(`Great, I'll get started on your ${orderName}  right away, it will cost  ${totalPrice} kr and pizzas will take ${orderTime} minutes`) 
 
 

71f5bb3d55417ca9f0550205926df8eb38322848
nasimmhn <nasim.mahzoun@gmail.com>
2019-10-17 22:55:44 +0200

    added div and form in HTML

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,27 +6,31 @@ const pizzaPrice = 80
 
 
 //Iteration2 - GREETING THE CUSTOMER 
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian},${hawaiian} and ${pepperoni}`)
+document.getElementById("greeting").innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian},${hawaiian} and ${pepperoni}`
+//alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian},${hawaiian} and ${pepperoni}`)
 
 
 //Iteration 5 - checking that the pizza is on the menu    //Iteration6 - part1
 //when checkorderName goes into the function trick the function to exit and go to validate var valid which checks if the pizzaName is on the menu.  
 const checkOrderName = (orderName) =>{
     if (orderName==vegetarian || orderName==hawaiian || orderName == pepperoni) {
-        return false
+        return true
     } else {
         alert('choose the right one')
-        return true
+        return false
     }
 }
-// valid makes the user put in a valid menu chocie to exit the while loop we return false in the checkOrderName.
-    let valid = true
-    while (valid) {
-    //Iteration3 - Ask the customer 
-    var orderName = prompt(`Enter the name of the pizza you want to order today!`) 
-    
-    valid = checkOrderName(orderName)
-}
+
+//checkOrderName(orderName)
+
+//Iteration3 - Ask the customer
+
+
+document.getElementById("ordername").innerHTML = `Enter the name of the pizza you want to order today!`
+
+
+
+
 
 
 //Iteration3 - Ask the customer 
